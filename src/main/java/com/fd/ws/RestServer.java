96acbd885033d07688f57418b7be3bfdea46489e
fudong <fudong@wowkai.cn>
2019-03-20 20:50:39 +0800

    sync

--- src/main/java/com/fd/ws/RestServer.java
+++ src/main/java/com/fd/ws/RestServer.java
@@ -48,40 +48,43 @@ public class RestServer {
 
 	@OnOpen
 	public void open(Session session, EndpointConfig config) {
-		this.reqInfo = (ReqInfo) config.getUserProperties().get(WsServerListener.REQ_INFO);
-		for (ClientInfo ci : CoordinateUtil.CLIENTS) {
-			if (ci.getSession() != session && !ci.getSession().getId().equals(session.getId()) && session.isOpen()) {
-				try {
-					session.getBasicRemote().sendObject(ci.getClientApi());
-				} catch (IOException | EncodeException e) {
-					e.printStackTrace();
+		synchronized (CoordinateUtil.CLIENTS) {
+			this.reqInfo = (ReqInfo) config.getUserProperties().get(WsServerListener.REQ_INFO);
+			for (ClientInfo ci : CoordinateUtil.CLIENTS) {
+				if (ci.getSession() != session && !ci.getSession().getId().equals(session.getId())
+						&& session.isOpen()) {
+					try {
+						session.getBasicRemote().sendObject(ci.getClientApi());
+					} catch (IOException | EncodeException e) {
+						e.printStackTrace();
+					}
 				}
 			}
-		}
-		timer.schedule(new TimerTask() {
-			@Override
-			public void run() {
-				try {
-					if (session.isOpen()) {
-						if (isAlive) {
-							isAlive = false;
-							session.getBasicRemote().sendPing(ByteBuffer.wrap(pings));
+			timer.schedule(new TimerTask() {
+				@Override
+				public void run() {
+					try {
+						if (session.isOpen()) {
+							if (isAlive) {
+								isAlive = false;
+								session.getBasicRemote().sendPing(ByteBuffer.wrap(pings));
+							} else {
+								cancel();
+								if (session.isOpen()) {
+									log.error("关闭half-open连接");
+									session.close(new CloseReason(CloseCodes.CLOSED_ABNORMALLY, "已经超时，请重新连接。"));
+								}
+							}
 						} else {
 							cancel();
-							if (session.isOpen()) {
-								log.error("关闭half-open连接");
-								session.close(new CloseReason(CloseCodes.CLOSED_ABNORMALLY, "已经超时，请重新连接。"));
-							}
 						}
-					} else {
-						cancel();
+					} catch (Exception e) {
+						e.printStackTrace();
+						log.error("heartbeat", e);
 					}
-				} catch (Exception e) {
-					e.printStackTrace();
-					log.error("heartbeat", e);
 				}
-			}
-		}, 50000, 50000);
+			}, 50000, 50000);
+		}
 	}
 
 	@OnError
@@ -103,51 +106,51 @@ public class RestServer {
 
 	@OnClose
 	public void close(Session session) {
-		HttpApiInfo ha = CoordinateUtil.getHttpApiInfo(session);
-		if (ha != null) {
-			log.error(String.format("客户端%s关闭连接..", ha.getBaseUrl()));
-			Iterator<ClientInfo> ite = CoordinateUtil.CLIENTS.iterator();
-			while (ite.hasNext()) {
-				ClientInfo disapicl = ite.next();
-				if (disapicl.getSession().getId().equals(session.getId())) {
-					ite.remove();
-					ClientApi clientApi = disapicl.getClientApi();
-					clientApi.getHttpApiInfo().setIsOnline(false);
-					sendapi(clientApi, session);
-					log.error(String.format("%s客户端销毁成功..", session.getId()));
-					log.error("还剩客户端总数量:{}", CoordinateUtil.CLIENTS.size());
+		synchronized (CoordinateUtil.CLIENTS) {
+			HttpApiInfo ha = CoordinateUtil.getHttpApiInfo(session);
+			if (ha != null) {
+				log.error(String.format("客户端%s关闭连接..", ha.getBaseUrl()));
+				Iterator<ClientInfo> ite = CoordinateUtil.CLIENTS.iterator();
+				while (ite.hasNext()) {
+					ClientInfo disapicl = ite.next();
+					if (disapicl.getSession().getId().equals(session.getId())) {
+						ite.remove();
+						ClientApi clientApi = disapicl.getClientApi();
+						clientApi.getHttpApiInfo().setIsOnline(false);
+						sendapi(clientApi, session);
+						log.error(String.format("%s客户端销毁成功..", session.getId()));
+						log.error("还剩客户端总数量:{}", CoordinateUtil.CLIENTS.size());
+					}
 				}
+			} else {
+				log.error("CoordinateUtil.CLIENTS:" + CoordinateUtil.CLIENTS.size());
 			}
-		} else {
-			log.error("CoordinateUtil.CLIENTS:" + CoordinateUtil.CLIENTS.size());
 		}
 	}
 
 	@OnMessage
 	public void handlerData(ClientApi api, Session session) {
-		if (api.getSync()) {
-			for (ApiInfo ai : api.getApis()) {
-				ClientApi ca = CoordinateUtil.getClientApiByApiInfo(ai.getName(), ai.getMethod());
-				if (ca != null && session.isOpen()) {
-					try {
-						session.getBasicRemote().sendObject(ca);
-						log.info("主动同步成功。。。。");
-					} catch (IOException | EncodeException e) {
-						e.printStackTrace();
+		synchronized (CoordinateUtil.CLIENTS) {
+			if (api.getSync()) {
+				for (ApiInfo ai : api.getApis()) {
+					ClientApi ca = CoordinateUtil.getClientApiByApiInfo(ai.getName(), ai.getMethod());
+					if (ca != null && session.isOpen()) {
+						try {
+							session.getBasicRemote().sendObject(ca);
+							log.info("主动同步成功。。。。:{}", ca);
+						} catch (IOException | EncodeException e) {
+							e.printStackTrace();
+						}
 					}
 				}
-			}
 
-		} else if (api.getHttpApiInfo() != null) {
-			if (api.getHttpApiInfo().getContextPath() != null) {
-				log.info(String.format("新增前，客户端总数量：%s", CoordinateUtil.CLIENTS.size()));
-				if (api.getHttpApiInfo().getHost() == null || api.getHttpApiInfo().getHost().trim().length() < 4) {
-					api.getHttpApiInfo().setHost(reqInfo.getRemoteAddr());
-				}
-				log.info(api.toString());
-				sendapi(api, session);
-				ClientInfo curClient = new ClientInfo(api, session);
-				synchronized (CoordinateUtil.CLIENTS) {
+			} else if (api.getHttpApiInfo() != null) {
+				if (api.getHttpApiInfo().getContextPath() != null) {
+					log.info(String.format("新增前，客户端总数量：%s", CoordinateUtil.CLIENTS.size()));
+					if (api.getHttpApiInfo().getHost() == null || api.getHttpApiInfo().getHost().trim().length() < 4) {
+						api.getHttpApiInfo().setHost(reqInfo.getRemoteAddr());
+					}
+					ClientInfo curClient = new ClientInfo(api, session);
 					if (api.getHttpApiInfo().getIsOnline()) {
 						CoordinateUtil.CLIENTS.add(curClient);
 						log.info(String.format("服务器%s上线", api.getHttpApiInfo().getBaseUrl()));
@@ -163,18 +166,19 @@ public class RestServer {
 						}
 						CoordinateUtil.CLIENTS.remove(curClient);
 					}
+					sendapi(api, session);
+					log.info(String.format("添加完毕，当前客户端总数量：%s", CoordinateUtil.CLIENTS.size()));
+				} else {
+					log.error(String.format("%s ContextPath is  null  ", api.getHttpApiInfo().getHost()));
 				}
-				log.info(String.format("添加完毕，当前客户端总数量：%s", CoordinateUtil.CLIENTS.size()));
 			} else {
-				log.error(String.format("%s ContextPath is  null  ", api.getHttpApiInfo().getHost()));
+				log.error("非法请求........................................");
 			}
-		} else {
-			log.error("非法请求........................................");
 		}
 	}
 
 	private void sendapi(ClientApi api, Session session) {
-		for (ClientInfo ci : CoordinateUtil.CLIENTS) {
+		CoordinateUtil.CLIENTS.parallelStream().forEach(ci->{
 			Session se = ci.getSession();
 			if (!se.getId().equals(session.getId()) && se.isOpen()) {
 				try {
@@ -188,8 +192,9 @@ public class RestServer {
 					e.printStackTrace();
 				}
 			}
-
-		}
+			
+		});
+		
 	}
 
 	private static Timer timer = new Timer(true);
